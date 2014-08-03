cd781101017096959d891a47ea1d2f1b63a828b8
Martin Disch <martindisch@gmail.com>
2014-08-03 18:20:07 +0200

    Started testing

---
+++ app/src/androidTest/java/com/martin/feeder/SourceTest.java
@@ -0,0 +1,31 @@
+package com.martin.feeder;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Martin on 03.08.2014.
+ */
+public class SourceTest extends AndroidTestCase {
+
+    private NewsSources nSources;
+    private NewsCollection nColl;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        nSources = new NewsSources(getContext());
+    }
+
+    public void testA3News() {
+
+    }
+    public void testA3Devhub() {
+        
+    }
+    public void testSpaceEngineers() {
+
+    }
+    public void testLayer() {
+
+    }
+}
