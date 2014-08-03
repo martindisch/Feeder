07ba16aa4c19822c3817da1ad2e4524a3a3c615c
Martin Disch <martindisch@gmail.com>
2014-08-03 10:40:46 +0200

    Prepared Fragments

---
+++ app/src/main/java/com/martin/feeder/NewsFragment.java
@@ -0,0 +1,6 @@
+package com.martin.feeder;
+
+import android.app.Fragment;
+
+public class NewsFragment extends Fragment {
+}
---
+++ app/src/main/java/com/martin/feeder/SourcesFragment.java
@@ -0,0 +1,6 @@
+package com.martin.feeder;
+
+import android.app.Fragment;
+
+public class SourcesFragment extends Fragment {
+}
