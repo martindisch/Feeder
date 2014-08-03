0e850affdd01d089f6da8b73d0529b6b7723d13e
Martin Disch <martindisch@gmail.com>
2014-08-03 10:32:35 +0200

    Reduced section count to 2

--- app/src/main/java/com/martin/feeder/Main.java
+++ app/src/main/java/com/martin/feeder/Main.java
@@ -131,7 +131,7 @@ public class Main extends Activity implements ActionBar.TabListener {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 2;
         }
 
         @Override
@@ -142,8 +142,6 @@ public class Main extends Activity implements ActionBar.TabListener {
                     return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
                     return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
         }
