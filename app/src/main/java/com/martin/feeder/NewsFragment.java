4b8568abe43802298c58dd9af892d47fa7c4b6e8
Martin Disch <martindisch@gmail.com>
2014-08-16 14:53:49 +0200

    Replaced 'No news' Toast with Snackbar

--- app/src/main/java/com/martin/feeder/NewsFragment.java
+++ app/src/main/java/com/martin/feeder/NewsFragment.java
@@ -1,13 +1,22 @@
 package com.martin.feeder;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.SharedPreferences;
+import android.graphics.Path;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class NewsFragment extends Fragment {
@@ -16,6 +25,8 @@ public class NewsFragment extends Fragment {
     public RecyclerView mList;
     private NewsSources nSources;
     private NewsCollection nColl;
+    private RelativeLayout mSnackbar;
+    private TextView mSnackButton;
 
     public String name = "NewsFragment";
 
@@ -23,6 +34,8 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_news, container, false);
         mList = (RecyclerView) v.findViewById(R.id.rvList);
+        mSnackButton = (TextView) v.findViewById(R.id.snackbar_button);
+        mSnackbar = (RelativeLayout) v.findViewById(R.id.snackbar);
         return v;
     }
 
@@ -34,6 +47,38 @@ public class NewsFragment extends Fragment {
         mList.setLayoutManager(layoutManager);
         nSources = new NewsSources(getActivity());
         loadUnread();
+
+        mSnackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hideSnackbar();
+            }
+        });
+
+        // Initialize Snackbar
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics());
+        mSnackbar.setY(mSnackbar.getY() + px);
+        mSnackbar.setVisibility(mSnackbar.VISIBLE);
+    }
+
+    private void showSnackbar() {
+        float px = -TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics());
+        moveSnackbar(px);
+    }
+
+    private void hideSnackbar() {
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics());
+        moveSnackbar(px);
+    }
+
+    private void moveSnackbar(float px) {
+        Path p = new Path();
+        ObjectAnimator sbAnimator;
+        p.moveTo(mSnackbar.getX(), mSnackbar.getY());
+        p.rLineTo(0, px);
+        sbAnimator = ObjectAnimator.ofFloat(mSnackbar, View.X, View.Y, p);
+        sbAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        sbAnimator.start();
     }
 
     @Override
@@ -65,7 +110,7 @@ public class NewsFragment extends Fragment {
                     public void run() {
                         mList.setAdapter(new SiteAdapter(nColl, ((Main) getActivity()).findFragmentByPosition(0), "NewsFragment"));
                         if (nColl.getTitles().length == 0) {
-                            Toast.makeText(getActivity(), "No news", Toast.LENGTH_SHORT).show();
+                           showSnackbar();
                         }
                         mCallback.actionFinished();
                         if (mCallback.changeVisibility()) {
