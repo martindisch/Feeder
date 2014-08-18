fb4499efef0da860869a5043d47028c4d94b563a
Martin Disch <martindisch@gmail.com>
2014-08-18 21:46:01 +0200

    Implemented tests for new NewsCollection

--- app/src/androidTest/java/com/martin/feeder/CollectionTest.java
+++ app/src/androidTest/java/com/martin/feeder/CollectionTest.java
@@ -5,12 +5,43 @@ import android.test.AndroidTestCase;
 public class CollectionTest extends AndroidTestCase {
 
     private NewsCollection testCollection;
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
+
+    private void prep() {
         String[] titles = { "Title 1", "Title 2", "Title 3", "Title 4" };
         String[] urls = { "Url1", "Url2", "Url3", "Url4" };
         String[] contents = { "C1", "C2", "C3", "C4" };
         testCollection = new NewsCollection(titles, urls, contents);
     }
+
+    public void testRemoveFirst() {
+        prep();
+        testCollection.removeItem(0);
+        assertEquals(testCollection.getTitles()[0], "Title 2");
+        assertEquals(testCollection.getTitles()[testCollection.getTitles().length - 1], "Title 4");
+    }
+
+    public void testRemoveLast() {
+        prep();
+        testCollection.removeItem(3);
+        assertEquals(testCollection.getTitles()[0], "Title 1");
+        assertEquals(testCollection.getTitles()[testCollection.getTitles().length - 1], "Title 3");
+    }
+
+    public void testRemoveMiddle() {
+        prep();
+        testCollection.removeItem(1);
+        assertEquals(testCollection.getTitles()[0], "Title 1");
+        assertEquals(testCollection.getTitles()[testCollection.getTitles().length - 1], "Title 4");
+        assertEquals(testCollection.getTitles()[1], "Title 2");
+    }
+
+    public void testRemoveTwo() {
+        prep();
+        testCollection.removeItem(0);
+        testCollection.removeItem(1);
+        assertEquals(testCollection.getTitles()[0], "Title 2");
+        assertEquals(testCollection.getTitles()[1], "Title 4");
+    }
+
+
 }
